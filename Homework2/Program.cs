af9221842cb8f1b42b7d7e829332e4db5c62e860
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-05-15 15:01:51 +0300

    Выполнена задача 1

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -10,6 +10,24 @@ namespace Homework2
     {
         static void Main(string[] args)
         {
+            Task1();
+            //Task2();
         }
+
+        /// <summary>
+        ///Задача 1. Запросить минимальную и максимальную температуру за сутки 
+        ///и вывести среднесуточную температуру
+        /// </summary>
+        public static void Task1()
+        {
+            Console.Write("Введите минимальную температуру за сутки ");
+            double t_min = double.Parse(Console.ReadLine());
+            Console.Write("Введите максимальную температуру за сутки ");
+            double t_max = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Среднесуточная температура равна {(t_max + t_min) / 2:F1} градусов");
+            Console.ReadKey();
+        }
+
+        
     }
 }
