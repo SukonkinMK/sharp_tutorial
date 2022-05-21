e21b9d8e2d2c799d9c08111a71dcbf374e71afec
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-05-21 10:33:27 +0300

    Создал меню

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -10,6 +10,34 @@ namespace Homework3
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("Введите номер задачи (от 1 до 6): ");
+                int number = int.Parse(Console.ReadLine());
+                switch (number)
+                {
+                    case 0:
+                        Console.WriteLine("Завершение работы приложения...");
+                        Console.ReadKey();
+                        return;
+                    case 1:
+                        Task1();
+                        break;
+                    case 2:
+                        Task2();
+                        break;
+                    case 3:
+                        Task3();
+                        break;
+                    case 4:
+                        Task4();
+                        break;
+                    default:
+                        Console.WriteLine("Некорректный номер задачи ...");
+                        Console.ReadKey();
+                        break;
+                }
+            }
         }
     }
 }
