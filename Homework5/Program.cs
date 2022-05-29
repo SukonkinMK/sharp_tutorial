6d8252e15148598167451f94b33a734636d4f5a0
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-05-29 14:35:54 +0300

    Сделал задачу 5

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Homework5
 {
@@ -18,9 +19,10 @@ namespace Homework5
                 Console.WriteLine("2 - Задача 2");
                 Console.WriteLine("3 - Задача 3");
                 Console.WriteLine("4 - Задача 4");
+                Console.WriteLine("5 - Задача 5");
                 Console.WriteLine("0 - Завершение работы приложения");
                 Console.WriteLine("===================================");
-                Console.Write("Введите номер задачи (от 1 до 4): ");
+                Console.Write("Введите номер задачи (от 1 до 5): ");
                 int number = int.Parse(Console.ReadLine());
                 switch (number)
                 {
@@ -39,6 +41,9 @@ namespace Homework5
                         break;
                     case 4:
                         Task4();
+                        break;
+                    case 5:
+                        Task5();
                         break;
                     default:
                         Console.WriteLine("Некорректный номер задачи ...");
@@ -169,6 +174,182 @@ namespace Homework5
             {
                 File.AppendAllText(filename, $"{indent}{ (i == files.Length - 1 ? "\u2514\u2500" : "\u251C\u2500") }{files[i].Name}\n");
             }
+        }
+
+        /// <summary>
+        /// Задача 5. Список задач
+        /// </summary>
+        public static void Task5()
+        {
+            string filename = "tasks.xml";
+            ToDo[] toDoList;
+            if (File.Exists(filename))
+            {
+                toDoList = ReadToDoList(filename);
+            }
+            else
+            {
+                Console.Write("Список дел пуст. Хотите добавить задачи? (y / n) ");
+                string ans = Console.ReadLine();
+                if(ans != "y" && ans != "n")
+                {
+                    Console.WriteLine("Некорректный ввод.");
+                    return;
+                }
+                else
+                {
+                    if(ans == "n") { return; }
+                    else
+                    {
+                        toDoList = CreateToDoList();
+                    }
+                }
+
+            }
+            PrintToDoList(toDoList);
+            while(true)
+            {
+                Console.Write($"Введите номер выполненной задачи (от 1 до {toDoList.Length}). Для выхода введите 0: ");
+                if(int.TryParse(Console.ReadLine(),out int toDoNumber))
+                {
+                    if(toDoNumber == 0)
+                    {
+                        break;
+                    }
+                    else if(toDoNumber >=1 && toDoNumber <= toDoList.Length)
+                    {
+                        toDoList[toDoNumber - 1].IsDone = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректный ввод.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод.");
+                }
+            }
+            SaveToDoList(toDoList,filename);
+        }
+
+        /// <summary>
+        /// Чтение списка дел из XNL-файла
+        /// </summary>
+        /// <param name="filename">Имя файла</param>
+        /// <returns>Массив списка дел</returns>
+        public static ToDo[] ReadToDoList(string filename)
+        {
+            string xml = File.ReadAllText(filename);
+            StringReader stringReader = new StringReader(xml);
+            XmlSerializer serializer = new XmlSerializer(typeof(ToDo[]));
+            return (ToDo[])serializer.Deserialize(stringReader);
+        }
+
+        /// <summary>
+        /// Сохранение массива дел в XML-файл
+        /// </summary>
+        /// <param name="list">Массив со списоком дел</param>
+        /// <param name="filename">Имя файла</param>
+        public static void SaveToDoList(ToDo[] list, string filename)
+        {
+            StringWriter stringWriter = new StringWriter();
+            XmlSerializer serializer = new XmlSerializer(typeof(ToDo[]));
+            serializer.Serialize(stringWriter, list);
+            string xml = stringWriter.ToString();
+            File.WriteAllText(filename, xml);
+        }
+
+        /// <summary>
+        /// Печать списка дел
+        /// </summary>
+        /// <param name="list">список дел</param>
+        public static void PrintToDoList(ToDo[] list)
+        {
+            Console.WriteLine("Текущий список дел:");
+            for(int i = 0; i < list.Length; i++)
+            {
+                if (list[i].IsDone)
+                {
+                    Console.Write("[x]");
+                }
+                else
+                {
+                    Console.Write("   ");
+                }
+                Console.WriteLine($"{i + 1}. {list[i].Title}");
+            }
+        }
+
+        /// <summary>
+        /// Добавление в список дел новой задачи
+        /// </summary>
+        /// <param name="list">Ссылка на текущий список дел</param>
+        /// <param name="name">Новая задача</param>
+        /// <returns>Список с новой задачей</returns>
+        public static ToDo[] AddWorkToList(ToDo[] list, string name)
+        {
+            ToDo[] finalList = new ToDo[list.Length+1];
+            for (int i = 0; i < finalList.Length; i++)
+            {
+                if(i != finalList.Length - 1) { finalList[i] = list[i]; }
+                else
+                {
+                    finalList[i] = new ToDo(name, false);
+                }
+            }
+            return finalList;
+        }
+
+        /// <summary>
+        /// Создание списка дел
+        /// </summary>
+        /// <returns>Массив ToDo</returns>
+        public static ToDo[] CreateToDoList()
+        {
+            ToDo[] toDoList = new ToDo[1];
+            Console.Write("Введите название задачи: ");
+            toDoList[0] = new ToDo(Console.ReadLine(), false);
+            while (true)
+            {
+                Console.Write("Добавить еще задачи? (y / n): ");
+                string addAns = Console.ReadLine();
+                if (addAns == "y")
+                {
+                    Console.Write("Введите название задачи: ");
+                    toDoList = AddWorkToList(toDoList, Console.ReadLine());
+                }
+                else if (addAns == "n")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод.");
+                }
+            }
+            return toDoList;
+        }
+    }
+    public class ToDo
+    {
+        string title;
+        bool isDone;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public bool IsDone
+        {
+            get { return isDone;}
+            set { isDone = value;}
+        }
+        public ToDo() { }
+        public ToDo(string title, bool status)
+        {
+            this.title = title;
+            this.isDone = status;
         }
     }
 }
